1d7e4bad49b4abd230db5cf54d32b93a0ee76e22
piegez <jessicamsampaio@gmail.com>
2024-06-22 11:27:31 -0300

    atualiacao

---
+++ src/styles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const cores = {
+  branca: '#eee',
+  preto: '#111',
+  cinza: '#333',
+  verde: '#10AC84',
+  cinzaClaro: '#A3A3A3'
+}
+
+export const GLobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    background-color: ${cores.preto};
+    color: ${cores.branca};
+    padding-top: 40px;
+  }
+    .container {
+    max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;}
+`
